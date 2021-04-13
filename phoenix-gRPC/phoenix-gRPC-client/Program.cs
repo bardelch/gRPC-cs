59c075b30cc9605a2c78aed907ba8c96caf37ccf
Chuck Bardel <chuck.bardel@gmail.com>
2021-04-13 15:41:10 -0400

    try to delay client for server startup

--- phoenix-gRPC/phoenix-gRPC-client/Program.cs
+++ phoenix-gRPC/phoenix-gRPC-client/Program.cs
@@ -22,7 +22,7 @@ namespace phoenix_gRPC
         {
             Console.WriteLine("Hello World!");
             // The port number(5001) must match the port of the gRPC server.
-            using var channel = GrpcChannel.ForAddress("https://localhost:49158");
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "GreeterClient" });
@@ -31,6 +31,7 @@ namespace phoenix_gRPC
         }
         static void Main(string[] args)
         {
+            Task.Delay(100000);
             HelloWorld().Wait();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
