66d1f46c77ab9f0aa009fae33bfa931586303c3d
Chuck Bardel <chuck.bardel@gmail.com>
2021-04-13 10:45:12 -0400

    added subproject for gRPC client

---
+++ phoenix-gRPC/phoenix-gRPC-client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*
+ * https://docs.microsoft.com/en-us/aspnet/core/tutorials/grpc/grpc-start?view=aspnetcore-3.0&tabs=visual-studio
+ * 
+ * From Visual Studio, select Tools > NuGet Package Manager > Package Manager Console
+ * From the Package Manager Console window, run cd phoenix-gRPC-client to change directories to the folder containing the phoenix-gRPC-client.csproj files.
+ * Run the following commands:
+ * Install-Package Grpc.Net.Client
+ * Install-Package Google.Protobuf
+ * Install-Package Grpc.Tools
+ */
+
+namespace phoenix_gRPC_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
