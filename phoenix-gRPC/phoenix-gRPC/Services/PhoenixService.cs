67736899d6d5a474ce00ff70f9b07b155da74f5f
Chuck Bardel <chuck.bardel@gmail.com>
2021-04-16 15:51:23 -0400

    added PhoenixService.cs to return fault

---
+++ phoenix-gRPC/phoenix-gRPC/Services/PhoenixService.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace phoenix_gRPC
+{
+    public class PLC_phoenixService : phoenixService.phoenixServiceBase
+    {
+        /*
+         * message phoenix_STATUS {
+         * bool READY		= 1;
+         * bool BUSY		= 2;
+         * bool DONE		= 3;
+         * bool PASS		= 4;
+         * bool FAIL		= 5;
+         * bool FAULTED	= 6;
+         * }
+        */
+        public override global::System.Threading.Tasks.Task<global::phoenix_gRPC.phoenix_STATUS> PLC_Request(global::phoenix_gRPC.PLC_message request, ServerCallContext context)
+        {
+            return Task.FromResult(new phoenix_STATUS
+            {
+                READY   = false,
+                BUSY    = false,
+                DONE    = false,
+                PASS    = false,
+                FAIL    = false,
+                FAULTED = true
+            });
+        }
+    }
+}
